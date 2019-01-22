3ee4e8fca6520065aceed4a4bb3966b242f20e12
Mayta Trigozo, G <g.mayta.trigozo@accenture.com>
2019-01-22 12:00:18 +0100

    added new webpack config for handel vendors

---
+++ webpack.prod.vendor.js
@@ -0,0 +1,21 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common');
+const path = require('path');
+
+const dist = path.resolve(__dirname, './dist');
+
+module.exports = merge(common, {
+  mode: 'production',
+  optimization: {
+    splitChunks: {
+      chunks(chunk) {
+        return chunk.name !== 'polyfills';
+      },
+      name: 'vendor'
+    }
+  },
+  output: {
+    path: dist,
+    filename: '[name].min.js'
+  }
+});
