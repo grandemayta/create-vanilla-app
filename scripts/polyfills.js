3b2099f5a0234c089f8608b1b5de401613687c2e
Mayta Trigozo, G <g.mayta.trigozo@accenture.com>
2019-05-31 23:45:38 +0200

    add support to webcomponents

---
+++ scripts/polyfills.js
@@ -0,0 +1,25 @@
+// Promise, CustomEvents, Symbol, Array.from and Object.Assign
+// will load by webcomponents-loader.js
+
+module.exports = [
+    // OTHERS
+    'whatwg-fetch',
+    'intersection-observer',
+
+    // ARRAYS
+    'core-js/fn/array/entries',
+    'core-js/fn/array/find',
+    'core-js/fn/array/find-index',
+    'core-js/fn/array/includes',
+    'core-js/fn/array/keys',
+    'core-js/fn/array/values',
+
+    // OBJECTS
+    'core-js/fn/object/entries',
+    'core-js/fn/object/values',
+
+    // STRINGS
+    'core-js/fn/string/ends-with',
+    'core-js/fn/string/includes',
+    'core-js/fn/string/starts-with'
+];
