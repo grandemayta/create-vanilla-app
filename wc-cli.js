034fcd7e88a896ba8f40a62da8985e3d4a82046b
Mayta Trigozo, G <g.mayta.trigozo@accenture.com>
2019-06-01 10:53:23 +0200

    add minimist to handle arguments

--- wc-cli.js
+++ wc-cli.js
@@ -3,11 +3,16 @@ const path = require('path');
 const clear = require('clear');
 const figlet = require('figlet');
 const chalk = require('chalk');
+const memFs = require('mem-fs');
+const memFsEditor = require('mem-fs-editor');
+const argv = require('minimist')(process.argv.slice(2));
 const questions = require('./questions');
+const mfsEditor = memFsEditor.create(memFs.create());
 
 class WebComponentsCLI {
   constructor() {
-    this.root = path.resolve(__dirname, './src');
+    this.src = path.resolve(__dirname, './wc-boilerplate');
+    this.dest = ''
   }
 
   printLogo() {
@@ -22,10 +27,26 @@ class WebComponentsCLI {
     );
   }
 
+  showError(message) {
+    console.log(chalk.red(`[WC CLI] Error: ${message}`));
+  }
+
   async load() {
-    await this.printLogo();
-    const anwsers = await inquirer.prompt(questions);
-    console.log('ANWERS:::::::::', anwsers);
+    if (argv['_'].length > 0) {
+      const name = argv['_'][0];
+      this.dest = path.resolve(__dirname, name);
+
+      await this.printLogo();
+    
+      const anwsers = await inquirer.prompt(questions);
+      console.log('ANWERS:::::::::', name, anwsers);
+      mfsEditor.copy(
+        `${this.src}/babel-config.js`,
+        `${this.staticPath}/${this.name}.html`
+      );
+    } else {
+      this.showError('Type the name of your application!');
+    }
   }
 }
 
